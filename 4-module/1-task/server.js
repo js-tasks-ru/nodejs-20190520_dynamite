8ee8ef57ea31e80cbe3e88d880a00b07dfa6276a
ivanov-v <sferamail33@yandex.ru>
2019-06-03 19:39:21 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,30 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs').promises;
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  const hasNesting = pathname.split('/').length > 1;
+
+  if (hasNesting) {
+    res.statusCode = 400;
+    res.end('nesting banned');
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      fs.readFile(filepath).then((file) => {
+        res.statusCode = 200;
+        res.end(file);
+      }).catch(() => {
+        res.statusCode = 404;
+        res.end('not found');
+      });
       break;
 
     default:
